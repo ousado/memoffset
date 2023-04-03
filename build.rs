43117d127b67b506a78cf965aa02b80a86960ede
ousado <ousado@users.noreply.github.com>
2023-04-03 17:49:42 +0200

    Update build.rs

--- build.rs
+++ build.rs
@@ -1,8 +1,9 @@
 extern crate autocfg;
 
 fn main() {
-    let ac = autocfg::new();
     /*
+    let ac = autocfg::new();
+    
     // Check for a minimum version for a few features
     if ac.probe_rustc_version(1, 20) {
         println!("cargo:rustc-cfg=tuple_ty");
